d8b5e1c58319925faf53815af62d0eb2dd377a9e
Leehyon Koh <leehyon@live.com>
2023-09-11 22:53:56 +0800

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,11 +59,11 @@ export default function Header() {
             when={iconTitle().title}
             fallback={
               <>
-                <span class="text-transparent font-extrabold bg-clip-text bg-gradient-to-r dark:from-yellow-300 from-yellow-600 dark:to-red-700 to-red-700 mr-1">
-                  ChatGPT
+                <span class="font-extrabold text-slate-7 dark:text-slate mr-1">
+                  Koh
                 </span>
                 <span class="ml-1 font-extrabold text-slate-7 dark:text-slate">
-                  Vercel
+                  ChatGPT
                 </span>
               </>
             }
